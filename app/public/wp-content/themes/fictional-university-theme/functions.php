f5f547d9fc616a4ddca583098cfc1d9eb168757d
Gavin Quirk <gavinquirk@gmail.com>
2018-10-22 17:59:27 -0700

    added page titles, links to home, about, privacy, and banner to pages

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,7 +1,4 @@
 
-
-
-
 <?php
 
 
@@ -9,9 +6,17 @@
         wp_enqueue_script('main_university_javascript', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
         wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
         wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-        wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+        wp_enqueue_style('university_main_styles', get_stylesheet_uri(), null, '1.0');
     }
 
     add_action('wp_enqueue_scripts', 'university_files');
+
+
+
+    function university_features() {
+        add_theme_support('title-tag');
+    }
+
+    add_action('after_setup_theme', 'university_features');
 
 ?>
