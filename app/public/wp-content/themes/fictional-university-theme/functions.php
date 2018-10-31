12c42ac9639855aa8d70d099c91703f74ef6d30a
Gavin Quirk <gavinquirk@gmail.com>
2018-10-31 14:40:36 -0700

    added event post type, events archive

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -24,12 +24,6 @@
 
     add_action('after_setup_theme', 'university_features');
 
-    function university_post_types() {
-        register_post_type('event', array(
-            'public' => true
-        ));
-    }
 
-    add_action('init', 'university_post_types');
 
 ?>
