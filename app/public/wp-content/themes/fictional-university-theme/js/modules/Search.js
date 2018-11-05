d19850f2bb1d2a4ed2425bdb506758fb97743d30
Gavin Quirk <gavinquirk@gmail.com>
2018-11-05 00:49:34 -0800

    combine post query data and page query data in search results

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -45,13 +45,16 @@ class Search {
 
   getResults() {
     $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
-      this.resultsDiv.html(`
-        <h2 class="search-overlay__section-title">General Information</h2>
-        ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
-          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-        ${posts.length ? '</ul>' : ''}
-      `);
-      this.isSpinnerVisible = false;
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
+        var combineResults = posts.concat(pages);
+        this.resultsDiv.html(`
+          <h2 class="search-overlay__section-title">General Information</h2>
+          ${combineResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
+           ${combineResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+          ${combineResults.length ? '</ul>' : ''}
+        `);
+        this.isSpinnerVisible = false;
+      });
     });
   }
 
