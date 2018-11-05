2fc78a563f79bbfebbea31a36a0eb36dfb3936dd
Gavin Quirk <gavinquirk@gmail.com>
2018-11-04 22:59:55 -0800

    fill resultsDiv with user query post data on search

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -43,8 +43,13 @@ class Search {
   }
 
   getResults() {
-    $.getJSON('http://localhost:3000/wp-json/wp/v2/posts?search=' + this.searchField.val(), function(posts) {
-      alert(posts);
+    $.getJSON('http://localhost:3000/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+      this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        <ul class="link-list min-list">
+          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+        </ul>
+      `);
     });
   }
 
