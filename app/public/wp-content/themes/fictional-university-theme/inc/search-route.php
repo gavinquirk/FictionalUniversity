d7a18a582d9b379b21b8cac8759e4687ec87086a
Gavin Quirk <gavinquirk@gmail.com>
2018-11-19 13:18:08 -0800

    professors api

--- app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -1,7 +1,9 @@
 <?php
 
+// Init Rest API - University Search
 add_action('rest_api_init', 'universityRegisterSearch');
 
+// University Search Route
 function universityRegisterSearch() {
   register_rest_route('university/v1', 'search', array(
     'methods' => WP_REST_SERVER::READABLE,
@@ -9,8 +11,26 @@ function universityRegisterSearch() {
   ));
 }
 
+// University Search Callback
 function universitySearchResults() {
-  return 'Congratulations, you created a route!';
+  $professors = new WP_Query(array(
+    'post_type' => 'professor'
+  ));
+
+    // Array of results
+    $professorResults = array();
+
+    // Loop through professors query, push only needed data into professorResults array
+    while($professors->have_posts()) {
+      $professors->the_post();
+      array_push($professorResults, array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    return $professorResults;
+
 }
 
 
